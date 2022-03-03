2adf9ded388821d99e9b591f4b0ca08825b676a2
kavitha <kavitha.pazhanee@hcl.com>
2022-03-03 10:17:11 +0530

    doctor initial commit
    doctor initial commit

--- src/main/java/com/econsult/RailrunnersHealthCareApplication.java
+++ src/main/java/com/econsult/RailrunnersHealthCareApplication.java
@@ -2,10 +2,14 @@ package com.econsult;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class RailrunnersHealthCareApplication {
     public static void main(String[] args) {
         SpringApplication.run(RailrunnersHealthCareApplication.class, args);
     }
+
 }
