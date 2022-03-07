ff0eebdf359dc3b7e80ba70d4523f7c8bc3b0681
satishbh1 <satishkumar96bh@gmail.com>
2022-03-07 17:42:51 +0530

    Added FeedbackRepository.java class

---
+++ src/main/java/com/econsult/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.econsult.repository;
+
+import com.econsult.model.Feedback;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends MongoRepository<Feedback, String> {
+
+}
